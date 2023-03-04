b102034ad72051c05078b543664d8e12f29fd60b
6310682791 <6310682791@student.tu.ac.th>
2023-03-04 15:17:31 +0700

    add correct check and formatting code

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -28,8 +28,7 @@ class MainActivity : ComponentActivity() {
             NumberGuessingGameTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
                     NumberGuessingGameScreen()
                 }
@@ -37,72 +36,86 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 var randNum: Int = (1..1000).random()
+var count = 0
+
 @Composable
-fun NumberGuessingGameScreen(){
-    var numGuess by remember { mutableStateOf("")}
+fun NumberGuessingGameScreen() {
+    var numGuess by remember { mutableStateOf("") }
     val Guess = numGuess.toIntOrNull() ?: 0
     val output = randomGuess(Guess, randNum)
 
-    Column (
+
+    Column(
         modifier = Modifier.padding(32.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(text = stringResource(R.string.title_game),
+    ) {
+        Text(
+            text = stringResource(R.string.title_game),
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
             textAlign = TextAlign.Center,
         )
         Spacer(Modifier.height(194.dp))
-        EnterNumber(value = numGuess, onValueChange = { numGuess = it})
-        Spacer(Modifier.height(160.dp))
+        EnterNumber(value = numGuess, onValueChange = { numGuess = it })
+        Spacer(Modifier.height(10.dp))
         Button(onClick = { randNum = (1..1000).random() }) {
             Text(text = stringResource(R.string.try_again), fontSize = 10.sp)
         }
         Spacer(Modifier.height(10.dp))
-        Text(text = stringResource(R.string.guess_output, output),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-            color = colorResource(R.color.grey)
-        )
 
+        if (Guess != randNum) {
+            Text(
+                text = stringResource(R.string.guess_output, output),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = colorResource(R.color.grey)
+            )
+            count++
+//            println(randNum)
+//            println(count)
 
+        } else {
+            Text(
+                text = stringResource(R.string.correct, count),
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+            count = 0
+        }
     }
 }
-
-
-
 
 @Composable
 fun EnterNumber(
     value: String,
     onValueChange: (String) -> Unit,
-){
+) {
     TextField(
         value = value,
         onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.input_guess))},
-        modifier = Modifier.fillMaxWidth( ),
+        label = { Text(stringResource(R.string.input_guess)) },
+        modifier = Modifier.fillMaxWidth(),
         singleLine = true,
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         colors = TextFieldDefaults.textFieldColors(
             backgroundColor = colorResource(R.color.white)
         )
-        )
+    )
 }
 
 private fun randomGuess(
-    Guess: Int,
-    randNum: Int
-):String {
+    Guess: Int, randNum: Int
+): String {
     var result: String = ""
-    if (Guess < randNum){
+    if (Guess > randNum) {
         result = " lower!"
-    }else if (Guess > randNum){
+    } else if (Guess < randNum) {
         result = " higher!"
-    }else{
+    } else {
         result = " correct!"
     }
     return result
