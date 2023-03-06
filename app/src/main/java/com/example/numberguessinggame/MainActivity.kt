7528c7c2996cff18a76150ec9d2ecbf742eb1472
Adelerd <Flook.as2544@gmail.com>
2023-03-06 18:07:48 +0700

    delete ton project and add flook project instant

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -1,128 +1,69 @@
 package com.example.numberguessinggame
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.numberguessinggame.ui.theme.NumberGuessingGameTheme
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlin.random.Random.Default.nextInt
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    lateinit var textView: TextView
+    lateinit var editText: EditText
+    lateinit var imageButtonCheck: ImageButton
+    lateinit var newGameButton: Button
+
+    var random: Int = nextInt(1,1000)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            NumberGuessingGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
-                ) {
-                    NumberGuessingGameScreen()
-                }
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
+        imageButtonCheck = findViewById(R.id.imageButtonCheck)
+        newGameButton = findViewById(R.id.newGameButton)
+
+        textView.text = "Please enter your guess:"
+
+        var timeClicked = 0
+
+        imageButtonCheck.setOnClickListener {
+
+            timeClicked += 1
+
+            val number: Int = editText.text.toString().toInt()
+
+            if (number < random) {
+
+                textView.text = "Hint: It's higher!"
+                editText.text.clear()
+
+            } else if (number > random) {
+
+                textView.text = "Hint: It's lower!"
+                editText.text.clear()
+
+            } else {
+
+                textView.text = "Collect! count: " + timeClicked.toString()
+
             }
         }
-    }
-}
 
-var randNum: Int = (1..1000).random()
-var count = 0
+        newGameButton.setOnClickListener{
 
-@Composable
-fun NumberGuessingGameScreen() {
-    var numGuess by remember { mutableStateOf("") }
-    val Guess = numGuess.toIntOrNull() ?: 0
-    val output = randomGuess(Guess, randNum)
+            reset()
 
-
-    Column(
-        modifier = Modifier.padding(32.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.title_game),
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center,
-        )
-        Spacer(Modifier.height(194.dp))
-        EnterNumber(value = numGuess, onValueChange = { numGuess = it })
-        Spacer(Modifier.height(10.dp))
-        Button(onClick = { randNum = (1..1000).random() }) {
-            Text(text = stringResource(R.string.try_again), fontSize = 10.sp)
         }
-        Spacer(Modifier.height(10.dp))
 
-        if (Guess != randNum) {
-            Text(
-                text = stringResource(R.string.guess_output, output),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                color = colorResource(R.color.grey)
-            )
-            count++
-//            println(randNum)
-//            println(count)
-
-        } else {
-            Text(
-                text = stringResource(R.string.correct, count),
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            )
-            count = 0
-        }
     }
-}
 
-@Composable
-fun EnterNumber(
-    value: String,
-    onValueChange: (String) -> Unit,
-) {
-    TextField(
-        value = value,
-        onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.input_guess)) },
-        modifier = Modifier.fillMaxWidth(),
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        colors = TextFieldDefaults.textFieldColors(
-            backgroundColor = colorResource(R.color.white)
-        )
-    )
-}
-
-private fun randomGuess(
-    Guess: Int, randNum: Int
-): String {
-    var result: String = ""
-    if (Guess > randNum) {
-        result = " lower!"
-    } else if (Guess < randNum) {
-        result = " higher!"
-    } else {
-        result = " correct!"
-    }
-    return result
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NumberGuessingGameTheme {
-        NumberGuessingGameScreen()
+    fun reset() {
+        random = nextInt(1,1000)
+        textView.text = "Please enter your guess:"
+        editText.text.clear()
     }
 }
